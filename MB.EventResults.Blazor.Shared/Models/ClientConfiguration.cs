c7b42bc98b30809732bd0809584b726f2e127f20
Magnus Bengtsson <nzmangan@hotmail.com>
2025-10-03 16:28:44 +0200

    Added client config removed some contants only applicable to the server.

---
+++ MB.EventResults.Blazor.Shared/Models/ClientConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace MB.EventResults.Blazor.Shared;
+
+public class ClientConfiguration {
+  public string BaseUrl { get; set; }
+  public string CompressionHeaderValue { get; set; }
+  public string ApiBaseUrl { get; set; }
+}
--- MB.EventResults.Blazor.Shared/Models/UrlConstants.cs
+++ MB.EventResults.Blazor.Shared/Models/UrlConstants.cs
@@ -1,11 +1,6 @@
 ﻿namespace MB.EventResults.Blazor.Shared;
 
-public class UrlConstants {
-  public const string Result = "/api/results";
-  public const string Stats = "/api/stats";
+public class UrlServerClientConstants {
   public const string GetClass = "/api/results/{id}";
   public const string Grades = "/api/grades";
-  public const string Import = "/api/import";
-  public const string Clear = "/api/clear";
-  public const string ReIndex = "/api/reindex";
 }
