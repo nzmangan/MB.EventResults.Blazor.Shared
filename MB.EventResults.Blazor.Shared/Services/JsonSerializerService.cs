982629ee5301c6446ac2764718906897ae75b1f8
Magnus Bengtsson <nzmangan@hotmail.com>
2025-10-04 21:37:13 +0200

    Bumped base library

--- MB.EventResults.Blazor.Shared/Services/JsonSerializerService.cs
+++ MB.EventResults.Blazor.Shared/Services/JsonSerializerService.cs
@@ -1,14 +1,14 @@
 ﻿namespace MB.EventResults.Blazor.Shared;
 
 public class JsonSerializerService : IJsonSerializerService {
-  private static readonly JsonSerializerOptions Options = new() { WriteIndented = true, PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+  private static readonly JsonSerializerOptions _Options = new() { WriteIndented = true, PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
 
   public T Deserialize<T>(string content) {
 
-    return JsonSerializer.Deserialize<T>(content, Options);
+    return JsonSerializer.Deserialize<T>(content, _Options);
   }
 
   public string Serialize<T>(T input) {
-    return JsonSerializer.Serialize(input, Options);
+    return JsonSerializer.Serialize(input, _Options);
   }
 }
